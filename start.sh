9a8c59d6588ecdbe021a5a8d9949e790748a3823
Kazukai • 剣 <136913040+TeamKazukai@users.noreply.github.com>
2023-08-29 12:07:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Lucyy 
 fi
-cd /Lucyy
+cd /THALAPATHY-FILTER-BOT 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
